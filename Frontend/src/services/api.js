6175d0147f558d0763fccbb1ce6462ed001fe0eb
Ayush <ayushmishra28114@gmail.com>
2026-01-27 00:06:23 +0530

    Frontend updated

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000/api";
-
 const api = axios.create({
-    baseURL : API_BASE_URL,
+  baseURL: "http://127.0.0.1:8000/api/",
 });
 
-api.interceptors.request.use((config => {
-    const AccessToken = localStorage.getItem("access_token");
-    if (AccessToken) {
-        config.headers.Authorization = `Bearer ${AccessToken}`;   
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
-}))
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
