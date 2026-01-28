903d1bc86d3dfdb8ddb3af09e6691e6528f45149
Ayush <ayushmishra28114@gmail.com>
2026-01-28 23:40:32 +0530

    Revert "Frontend updated"
    This reverts commit 6175d0147f558d0763fccbb1ce6462ed001fe0eb.

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,18 +1,17 @@
 import axios from "axios";
 
+const API_BASE_URL = "http://127.0.0.1:8000/api";
+
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+    baseURL : API_BASE_URL,
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("accessToken");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use((config => {
+    const AccessToken = localStorage.getItem("access_token");
+    if (AccessToken) {
+        config.headers.Authorization = `Bearer ${AccessToken}`;   
     }
     return config;
-  },
-  (error) => Promise.reject(error)
-);
+}))
 
 export default api;
