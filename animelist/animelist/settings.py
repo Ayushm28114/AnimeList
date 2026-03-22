d887ac7f506fead1aa9991402f4ab10a51295c50
Ayush <ayushmishra28114@gmail.com>
2026-03-23 00:16:07 +0530

    debug back to false

--- animelist/animelist/settings.py
+++ animelist/animelist/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1', 'yes')
 
 ALLOWED_HOSTS = [
     'animelist-g1eo.onrender.com', 
