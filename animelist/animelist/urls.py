285c258af6dac00ad967da8eae74c1f1da1f0ee6
Ayush <ayushmishra28114@gmail.com>
2026-03-27 22:40:53 +0530

    added health check page to keep the render alive

--- animelist/animelist/urls.py
+++ animelist/animelist/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 def health_check(request):
